b1fe5d5a322564dea567e6d5b6a46256b70ebe9f
vladyslavB <Krutmeka9>
2019-11-21 09:44:58 +0200

    Slider and interviewrs amount filtartion

--- src/MapApp.js
+++ src/MapApp.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import MapSettings from "./components/MapSettings";
 import Map from "./components/Map";
+import cloneDeep from "lodash-es/cloneDeep";
 
 class MapApp extends React.Component{
     constructor(props) {
         super(props);
 
         this.state = {
-            interviewers: [
+            interviewersDefault: [
                 { id: 1, name: "Test Interviewer 1", selected: false },
                 { id: 2, name: "Test Interviewer 2", selected: false },
                 { id: 3, name: "Test Interviewer 3", selected: false },
@@ -17,12 +18,38 @@ class MapApp extends React.Component{
                 { id: 6, name: "Test Interviewer 6", selected: false },
                 { id: 7, name: "Test Interviewer 7", selected: false },
                 { id: 8, name: "Test Interviewer 8", selected: false },
-            ]
+            ],
+            interviewers: [],
+            interviewersAmount: 30
         }
     }
 
+    componentDidMount() {
+        this.setState(prevState => ({
+            ...prevState,
+            interviewers: cloneDeep(this.state.interviewersDefault)
+        }))
+    }
+    
+    handleSliderChange = (event, newValue) => {
+        let interviewers = this.getLimitedRandomElements(this.state.interviewersDefault, newValue);
+        interviewers.forEach(int => int.selected = false);
+
+        this.setState(prevState => ({
+            ...prevState,
+            interviewers,
+            interviewersAmount: newValue
+        }))
+    };
+
+    getLimitedRandomElements  = (array, limit) => {
+        const shuffled = array.sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, limit);
+    };
+
     handleInterviewerSelect = (id) => {
         this.setState(prevState => ({
+            ...prevState,
             interviewers: prevState.interviewers.map(
                 int => int.id === id ? { ...int, selected: !int.selected } : int
             )
@@ -35,8 +62,11 @@ class MapApp extends React.Component{
                 <Grid container>
                     <Grid item container xs={4}>
                         <MapSettings
+                            maxInterviewers={this.state.interviewersDefault.length}
+                            interviewersAmount={this.state.interviewersAmount}
                             interviewers={this.state.interviewers}
                             handleSelectInterviewer={this.handleInterviewerSelect}
+                            handleSliderChange={this.handleSliderChange}
                         />
                     </Grid>
                     <Grid item container xs={8}>
--- src/components/MapSettings.js
+++ src/components/MapSettings.js
@@ -4,31 +4,47 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import InterviewersSlider from "./InterviewersSlider";
+import './mapSettings.css';
 
-const MapSettings = ({ interviewers, handleSelectInterviewer }) => {
+const MapSettings = ({
+                         interviewers,
+                         handleSelectInterviewer,
+                         handleSliderChange,
+                         interviewersAmount,
+                         maxInterviewers
+                }) => {
     return (
-      <List>
-          {
-              interviewers.map((interviewer) => {
-                  const labelId = `checkbox-list-label-${interviewer.id}`;
-                    return(
-                        <ListItem key={interviewer.id} dense button onClick={handleSelectInterviewer.bind(this, interviewer.id)}>
-                            <ListItemIcon>
-                                <Checkbox
-                                    edge="start"
-                                    checked={interviewer.selected}
-                                    tabIndex={-1}
-                                    disableRipple
-                                    inputProps={{ 'aria-labelledby': labelId }}
-                                />
-                            </ListItemIcon>
-                            <ListItemText id={labelId} primary={interviewer.name} />
-                        </ListItem>
-                    )
+        <div className="interviewers-settings">
+            <InterviewersSlider
+                handleSliderChange={handleSliderChange}
+                interviewersAmount={interviewersAmount}
+                maxInterviewers={maxInterviewers}
+            />
+            <List>
+                {
+                    interviewers.map((interviewer) => {
+                        const labelId = `checkbox-list-label-${interviewer.id}`;
+                        return(
 
-              })
-          }
-      </List>
+                            <ListItem key={interviewer.id} dense button onClick={handleSelectInterviewer.bind(this, interviewer.id)}>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={interviewer.selected}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        inputProps={{ 'aria-labelledby': labelId }}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText id={labelId} primary={interviewer.name} />
+                            </ListItem>
+                        )
+
+                    })
+                }
+            </List>
+        </div>
     );
 }
 
