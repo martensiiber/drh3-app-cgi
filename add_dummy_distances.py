9ff52ccd6c86db84ba9c77bbd74b6a50ed91fbc0
Kuhi <jaagup.kuhi@cgi.com>
2019-11-21 16:34:25 +0200

    added is_visited and dummy distances to adr_valim_xy_aggr.json

--- add_dummy_distances.py
+++ add_dummy_distances.py
@@ -1,8 +1,12 @@
 import json
-path = "src/data/adr_valim_xy.json"
+import random
+
+path = "src/data/adr_valim_xy_aggr.json"
 with open(path) as f:
     data = json.load(f)
     for row in data:
+        # 33
+        row['is_visited'] = random.random()<0.33
         row['kaugused'] = [{'sisendaadressi_id': 1, 'kaugus': 1.2}, {'sisendaadressi_id': 2, 'kaugus': 0.124}, {'sisendaadressi_id': 6, 'kaugus': 5.8},]
     
 with open(path, 'w') as f:
