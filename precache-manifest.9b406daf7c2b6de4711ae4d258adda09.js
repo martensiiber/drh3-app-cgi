7a781780f97d9a29154c99d68d1dca92b02368c0
Marten Siiber <martensiiber@gmail.com>
2019-11-22 14:15:01 +0200

    Updates

---
+++ precache-manifest.9b406daf7c2b6de4711ae4d258adda09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df64fba98a44d965e4702515a88fa216",
+    "url": "/drh3-app-cgi/index.html"
+  },
+  {
+    "revision": "2fe48df4f52531de3e35",
+    "url": "/drh3-app-cgi/static/css/2.7997c105.chunk.css"
+  },
+  {
+    "revision": "3e9849bfba0de9cc79ea",
+    "url": "/drh3-app-cgi/static/css/main.8c3c6ab7.chunk.css"
+  },
+  {
+    "revision": "2fe48df4f52531de3e35",
+    "url": "/drh3-app-cgi/static/js/2.6dc9f3a7.chunk.js"
+  },
+  {
+    "revision": "3e9849bfba0de9cc79ea",
+    "url": "/drh3-app-cgi/static/js/main.8b52ee82.chunk.js"
+  },
+  {
+    "revision": "5d17e7c4ce04458d3c14",
+    "url": "/drh3-app-cgi/static/js/runtime-main.52798eea.js"
+  },
+  {
+    "revision": "2bbf75138fbd592d9a8cbabbdf57e267",
+    "url": "/drh3-app-cgi/static/media/bg.2bbf7513.png"
+  },
+  {
+    "revision": "ad65451053a2a808edac7573ce65454c",
+    "url": "/drh3-app-cgi/static/media/logo.ad654510.svg"
+  }
+]);
